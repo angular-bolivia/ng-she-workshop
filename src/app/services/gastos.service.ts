f1e652638793eb1e9ee2e0c2c6b14d2c7c0950b3
Brian Vega <dentvega6@gmail.com>
2020-11-15 18:43:42 -0400

    - Se agrego el servicio de gastos

--- src/app/services/gastos.service.ts
+++ src/app/services/gastos.service.ts
@@ -1,21 +1,31 @@
 import { Injectable } from '@angular/core';
 import { Gasto } from '../gasto.model';
+import { StorageService } from './storage.service';
+
+const gastosStorageKey = 'Gasto_List';
+
+const defaultGastos: Gasto[] = [
+  {nombreGasto: 'Primer gasto', cantidadGasto: 100},
+  {nombreGasto: 'Segundo gasto', cantidadGasto: 50},
+  {nombreGasto: 'Tercer gasto', cantidadGasto: 200},
+  {nombreGasto: 'Cuarto gasto', cantidadGasto: 200},
+  {nombreGasto: 'Quinto gasto', cantidadGasto: 10},
+  {nombreGasto: 'Sexto gasto', cantidadGasto: 20},
+];
 
 @Injectable({
   providedIn: 'root'
 })
 export class GastosService {
+  gastos: Gasto[];
 
-  private gastos: Gasto[] = [
-    {nombreGasto: 'Primer gasto', cantidadGasto: 100},
-    {nombreGasto: 'Segundo gasto', cantidadGasto: 50},
-    {nombreGasto: 'Tercer gasto', cantidadGasto: 200},
-    {nombreGasto: 'Cuarto gasto', cantidadGasto: 200},
-    {nombreGasto: 'Quinto gasto', cantidadGasto: 10},
-    {nombreGasto: 'Sexto gasto', cantidadGasto: 20},
-  ];
+  constructor(private storageService: StorageService) {
+    this.gastos = storageService.getData(gastosStorageKey) || defaultGastos;
+  }
 
-  constructor() { }
+  guardarGastos(): void {
+    this.storageService.setData(gastosStorageKey, this.gastos);
+  }
 
   getGastos(): Gasto[] {
     return this.gastos;
@@ -23,5 +33,18 @@ export class GastosService {
 
   agregarGasto(gasto: Gasto): void {
     this.gastos.push(gasto);
+    this.guardarGastos();
+  }
+
+  actualizarGastos(item, changes): void {
+    const index = this.gastos.indexOf(item);
+    this.gastos[index] = { ...item, ...changes };
+    this.guardarGastos();
+  }
+
+  eliminarGasto(item): void {
+    const index = this.gastos.indexOf(item);
+    this.gastos.splice(index, 1);
+    this.guardarGastos();
   }
 }
